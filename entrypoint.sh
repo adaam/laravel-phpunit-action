1b4e4b5230535cb82c019bfce530f1c0950e96ae
adaam <adam.cws@gmail.com>
2023-02-08 18:35:46 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
 composer install --prefer-dist
+apk add --no-cache unzip
+apk add --no-cache --virtual .build-deps autoconf g++ make && pecl install redis && apk del .build-deps
+docker-php-ext-configure pcntl --enable-pcntl && docker-php-ext-install pcntl pdo pdo_mysql && docker-php-ext-enable redis && rm -rf /tmp/pear
 
 cp .env.example .env
 
