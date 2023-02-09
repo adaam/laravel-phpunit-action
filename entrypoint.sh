119662364e46c78de0d5c57a5bd1b6a8a134e007
adaam <adam.cws@gmail.com>
2023-02-09 15:00:41 +0800

    add xdebug and coverage report

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -x
 
 curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
 apk add --no-cache unzip
-apk add --no-cache --virtual .build-deps autoconf g++ make && pecl install redis && apk del .build-deps
+apk add --no-cache --virtual .build-deps autoconf g++ make && pecl install redis xdebug && apk del .build-deps
 docker-php-ext-configure pcntl --enable-pcntl && docker-php-ext-install pcntl pdo pdo_mysql && docker-php-ext-enable redis && rm -rf /tmp/pear
 
 composer install --prefer-dist
@@ -14,4 +14,4 @@ pwd
 ls -la
 php ./artisan key:generate
 
-php vendor/bin/phpunit
+php vendor/bin/phpunit --coverage-text
